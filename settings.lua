b5e689da0227a0c4261b6e48aee97f4ac5321539
Mark Sanders <mark.something@gmail.com>
2017-05-28 02:41:31 +0200

    First version.

---
+++ settings.lua
@@ -0,0 +1,16 @@
+data:extend({
+	-- Startup
+	{
+		type = "int-setting",
+		name = "Noxys_StackSizeMultiplier-multiplier",
+		setting_type = "startup",
+		minimum_value = 1,
+		default_value = 20,
+		maximum_value = 1000,
+		order = "a",
+	},
+	-- Global
+
+	-- Per user
+	
+})
